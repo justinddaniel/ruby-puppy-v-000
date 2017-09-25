1861487f52f742d29f8cc1916fd190f9f55788ba
Justin Daniel <justinddaniel@gmail.com>
2017-09-25 20:31:55 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << name 
+  end
+
+  def self.clear_all
+    @@all = []
+  end
+
+  def self.all
+    @@all.each do |dog|
+      puts "#{dog}"
+    end
+  end
+
+end
